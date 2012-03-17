b74b1362195f8074f2249743903e7279ee862d46
khlieng <kk_998@hotmail.com>
2012-03-17 01:57:27 +0100

    Skulle vært med i forrige commit, databasetilkoblingen dratt ut i en egen fil som kan inkluderes der den trengs

---
+++ dbconn.php
@@ -0,0 +1,4 @@
+<?php
+mysql_connect('kark.hin.no', 'gruppe6', 'R2weWXNnpRjacTdq');
+mysql_select_db('gruppe6');
+?>
---
+++ register.php
@@ -0,0 +1,6 @@
+<?php
+include('dbconn.php');
+
+mysql_close();
+header("location:index.php");
+?>
